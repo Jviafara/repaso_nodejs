8164aed4565f08d3a8e7d135f1e11a099f910b56
Jesús Viafara <jealvito@gmail.com>
2024-12-18 17:55:02 -0500

    18/12/2024

---
+++ 1.os-info.mjs
@@ -0,0 +1,20 @@
+import {
+    arch,
+    cpus,
+    freemem,
+    platform,
+    release,
+    totalmem,
+    uptime,
+} from 'node:os';
+
+console.log('Informacion del sistema opperativo');
+console.log('__________________________________');
+
+console.log('Nombre de sistema operativo:', platform());
+console.log('Versio del sistema operativo:', release());
+console.log('Arquitectura:', arch());
+console.log('CPUs:', cpus());
+console.log('Memoria Libre:', freemem() / 1024 / 1024);
+console.log('Memoria total:', totalmem() / 1024 / 1024);
+console.log('uptime', uptime() / 60 / 60);
---
+++ 2.file-system.js
@@ -0,0 +1,9 @@
+const fs = require('node:fs'); //A partir de node 16 user siempre el prefijo node:
+
+const stats = fs.statSync('./file.txt');
+console.log(
+    stats.isFile(),
+    stats.isDirectory(),
+    stats.isSymbolicLink(),
+    stats.size //tamaño en bytes
+);

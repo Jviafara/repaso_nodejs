b13c911d06dda5c5cdca08fd8c0a0a5e2ff8467b
Jesús Viafara <jealvito@gmail.com>
2024-12-19 17:31:06 -0500

    clase 1

---
+++ clase 1/9.http.js
@@ -0,0 +1,19 @@
+const http = require('node:http')
+const dotenv = require('dotenv').config()
+const { findAvailablePort } = require('./10.free-port')
+
+const desiredPort = process.env.PORT || 3000
+console.log(process.env.PORT)
+
+const server = http.createServer((req, res) => {
+  console.log('request received')
+  res.end('Hello World')
+})
+
+findAvailablePort(desiredPort).then(port => {
+  server.listen(port, () => {
+    console.log(
+    `Server is running on port http://localhost:${port}`
+    )
+  })
+})

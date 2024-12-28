f305842f70c882870b6af35881961089be0bbe39
Jesús Viafara <jealvito@gmail.com>
2024-12-28 01:49:03 -0500

    Clase 3 finalizada y comienzo de clase 4

--- clase 3/app.js
+++ clase 3/app.js
@@ -1,10 +1,92 @@
 const express = require('express')
+const movies = require('./movies.json')
+const crypto = require('node:crypto')
+const cors = require('cors')
+const { validateMovie, validatepartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.disable('x-powered-by')
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' })
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = ['http://localhost:3485', 'https://my-app.com']
+
+      if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+        return callback(null, true)
+      }
+    }
+  })
+)
+
+// Fetch all movies
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLocaleLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+// Fetch a movie by id
+app.get('/movies/:id', (req, res) => {
+  //path to regexp
+  const { id } = req.params
+  const movie = movies.find((movie) => movie.id === id)
+  if (!movie) return res.status(404).json({ message: 'Movie not found' })
+  res.status(200).json(movie)
+})
+
+// Create a new movie
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error)
+    return res.status(400).json({ message: result.error.message })
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+// Update a movie using PATCH
+app.patch('/movies/:id', (req, res) => {
+  const result = validatepartialMovie(req.body)
+
+  if (!result.success)
+    return res.status(400).json({ message: result.error.message })
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+  if (movieIndex === -1)
+    return res.status(404).json({ message: 'Movie not found' })
+
+  const updateMovie = { ...movies[movieIndex], ...result.data }
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+  if (movieIndex === -1)
+    return res.status(404).json({ message: 'Movie not found' })
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
 })
 
 const port = process.env.PORT ?? 8080
---
+++ clase 3/schemas/movies.js
@@ -0,0 +1,37 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string().nonempty(),
+  year: z.number().int().positive(),
+  director: z.string().nonempty(),
+  duration: z.number().int().positive(),
+  poster: z.string().url(),
+  genre: z.array(
+    z.enum([
+      'Action',
+      'Adventure',
+      'Crime',
+      'Comedy',
+      'Drama',
+      'Fantasy',
+      'Horror',
+      'Thriller',
+      'Sci-Fi'
+    ]),
+    {
+      required_error: 'Movie genre is required.',
+      invalid_type_error: 'Movie genre must be an array of enum Genre'
+    }
+  ),
+  rate: z.number().min(0).max(10).default(0)
+})
+
+function validateMovie(object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatepartialMovie(object) {
+  return movieSchema.partial().safeParse(object)
+}
+
+module.exports = { validateMovie, validatepartialMovie }
